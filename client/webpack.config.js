4c9ec81aa23efadf86befff58a2e6c5d793415f0
kanghonggu <wckhg89@gmail.com>
2018-02-13 13:32:17 +0900

    Add multi module

---
+++ client/webpack.config.js
@@ -0,0 +1,58 @@
+const webpack = require('webpack');
+const path = require('path');
+const webpackMerge = require('webpack-merge');
+const CommonsChunkPlugin = require("webpack/lib/optimize/CommonsChunkPlugin");
+
+const config = {
+    entry: {
+        app: path.resolve(__dirname, 'static/guppy/js/App.js'),
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: [{
+                    loader: 'babel-loader',
+                    options: {
+                        presets: [
+                            ['es2015', {moduels: false}]
+                        ]
+                    }
+                }]
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            },
+            {
+                test: /.(ttf|otf|eot|svg|woff(2)?)(\?[a-z0-9]+)?$/,
+                use: [{
+                    loader: 'file-loader'
+                }]
+            },
+            {
+                test: /\.html$/,
+                loader: 'mustache-loader'
+            }
+        ]
+    },
+    plugins : [
+        new webpack.ProvidePlugin({
+            $ : "jquery",
+            jQuery : "jquery",
+            'window.jQuery': 'jquery',
+            Backbone : "backbone",
+            _ : "underscore"
+        }),
+        new CommonsChunkPlugin({
+            name: "common",
+            filename: "common.js",
+            minChunks: Infinity
+        })
+    ]
+};
+
+module.exports = function(env) {
+    return webpackMerge(config, require(`./webpack.${env}.js`))
+};
